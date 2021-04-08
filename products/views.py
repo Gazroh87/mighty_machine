c33bdff36d05e93bfb97a72551e5b5f65eb121e0
Gary Hughes <gary.hughes87@virginmedia.com>
2021-04-08 14:29:01 +0000

    Added products view and templates, and fixed logos to load correctly

--- products/views.py
+++ products/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def all_products(request):
-    """ A view to display all products """
+    """A view to display all products, including sorting and search queries"""
 
-    return render(request, 'products/products.html')
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
