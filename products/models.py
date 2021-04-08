68d52d0c8d2198d8ae8a6b59084f154a29f9018a
Gary Hughes <gary.hughes87@virginmedia.com>
2021-04-08 12:17:58 +0000

    Customized admin

--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -13,15 +17,17 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=12)
     brand = models.CharField(max_length=254)
     part_name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=3, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    part_type = models.CharField(max_length=254) 
+    part_type = models.CharField(max_length=254)
 
     def __str__(self):
-            return self.part_name
+        return self.part_name
