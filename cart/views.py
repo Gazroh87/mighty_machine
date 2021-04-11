e483dcff93cbdfc261b69194ee21b23723c72fee
Gary Hughes <gary.hughes87@virginmedia.com>
2021-04-11 15:09:12 +0000

    Finished product colour logic

--- cart/views.py
+++ cart/views.py
@@ -14,12 +14,24 @@ def add_to_cart(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    colour = None
+    if 'product_colour' in request.POST:
+        colour = request.POST['product_colour']
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    if colour:
+        if item_id in list(cart.keys()):
+            if colour in cart[item_id]['items_by_colour'].keys():
+                cart[item_id]['items_by_colour'][colour] += quantity
+            else:
+                cart[item_id]['items_by_colour'][colour] = quantity
+        else:
+            cart[item_id] = {'items_by_colour': {colour: quantity}}
     else:
-        cart[item_id] = quantity
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
 
     request.session['cart'] = cart
     return redirect(redirect_url)
