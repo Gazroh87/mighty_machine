b75e978124fb5029fbf13ce2215b785741b06622
Gary Hughes <gary.hughes87@virginmedia.com>
2021-04-12 12:55:58 +0000

    Added modify cart and remove from cart views, and calc subtotal template filter

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -35,3 +35,52 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def mod_cart(request, item_id):
+    """ Modify the quantity of the specific product to the specific amount """
+
+    quantity = int(request.POST.get('quantity'))
+    colour = None
+    if 'product_colour' in request.POST:
+        colour = request.POST['product_colour']
+    cart = request.session.get('cart', {})
+
+    if colour:
+        if quantity > 0:
+            cart[item_id]['items_by_colour'][colour] = quantity
+        else:
+            del cart[item_id]['items_by_colour'][colour]
+            if not cart[item_id]['items_by_colour']:
+                cart.pop(item_id)
+    else:
+        if quantity > 0:
+            cart[item_id] = quantity
+        else:
+            cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """ Remove the item from the shopping cart """
+
+    try:
+        colour = None
+        if 'product_colour' in request.POST:
+            colour = request.POST['product_colour']
+        cart = request.session.get('cart', {})
+
+        if colour:
+            del cart[item_id]['items_by_colour'][colour]
+            if not cart[item_id]['items_by_colour']:
+                cart.pop(item_id)
+        else:
+            cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
