d2a618b339f6327d73af38c2469df36529f4ff32
Gary Hughes <gary.hughes87@virginmedia.com>
2021-04-14 12:05:44 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51ISktfAxHa4qHwIHggotPpqkLoVHuaUPYfxIKPKMeG5HwXnzy5DeBRkteLz3SvqtCENQKY7bNZua7A70cVxhhgOg00KbMCz1rT',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
