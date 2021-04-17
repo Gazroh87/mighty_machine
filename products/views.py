890543d053ab4db5f338627191cb04e331bfbce2
Gary Hughes <gary.hughes87@virginmedia.com>
2021-04-17 15:38:59 +0000

    Finished delete product functionality

--- products/views.py
+++ products/views.py
@@ -77,8 +77,9 @@ def add_product(request):
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(
                 request, 'Failed to add item. Ensure the form is valid.')
@@ -116,3 +117,15 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Successfully deleted product!')
+    return redirect(reverse('products'))
